c0fd692f6e5231692f81c311fb495b3ce1f496fc
Amar Mujak <142358633+amarmujak23@users.noreply.github.com>
2026-07-17 15:42:14 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Threading.Channels;
+
+string intro = "Calculator C# 1.0";
+
+// Parsing in C# converts a string to a number.
+
+//This line calls a dictionary that contains the operations available in the calculator.
+Dictionary<int, string> operations = new Dictionary<int, string>()
+{
+    {1, "Addition"},
+    {2, "Subtraction"},
+    {3, "Multiplication"},
+    {4, "Division"},
+    {5, "Square Root"}
+};
+
+
+//Prints menu 
+
+foreach (var operation in operations)
+{
+    Console.WriteLine(operation);
+}
+
+
+
+
+
+
+
+/*
+
+int number1 = 0;
+int number2 = 0;
+
+Console.WriteLine("Enter the first number: ");
+number1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter the second number: ");
+number2 = int.Parse(Console.ReadLine());
+
+
+int sum = number1 + number2;
+
+Console.WriteLine("The sum is: "  + sum + " when adding!"); 
+*/
