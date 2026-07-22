ed2e7d7dace987e4c46210f3b41af999afefbc22
Amar <amujak23@gmail.com>
2026-07-22 07:39:54 -0600

    Added switch statment and user input logic

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Channels;
+﻿using System.Numerics;
+using System.Threading.Channels;
 
 string intro = "Calculator C# 1.0";
 
@@ -22,11 +23,64 @@ foreach (var operation in operations)
     Console.WriteLine(operation);
 }
 
+Console.WriteLine("--------------------");
+
+// Using double data type to allow for decimal numbers
+double number1 = 0;
+double number2 = 0;
+
+
+Console.WriteLine("Select an operation by entering the corresponding number: ");
+
+int choice = int.Parse(Console.ReadLine());
+// string concatination
+Console.WriteLine("You selected: " + operations[choice]);
+
+Console.WriteLine("Enter the first number: ");
+number1 = double.Parse(Console.ReadLine());
+Console.Clear();
+
+Console.WriteLine("Enter the second number: ");
+number2 = double.Parse(Console.ReadLine());
+Console.Clear();
+
+
+
+// Using switch statement to handle the selected operation from dictionary
+switch (choice)
+{
+    case 1:
+        double add = number1 + number2;
+        Console.WriteLine($"The result of {operations[choice]} is: {add}");
+        break;
+    case 2:
+        double sub = number1 - number2;
+        Console.WriteLine($"The result of {operations[choice]} is: {sub}");
+        break;
+    case 3:
+        double mul = number1 * number2;
+        Console.WriteLine($"The result of {operations[choice]} is: {mul}");
+        break;
+    case 4:
+        double div = number1 / number2;
+        Console.WriteLine($"The result of {operations[choice]} is: {div}");
+        break;
+    case 5:
+        double sqrt = Math.Sqrt(number1);
+        Console.WriteLine($"The result of {operations[choice]} is: {sqrt}");
+        break;
+}
+
+
+
+// string interpolation
+// Console.WriteLine($"You selected: {operations[choice]}");
 
 
 
 
-
+// Testing key
+Console.ReadKey();
 
 /*
 
